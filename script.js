64c756f56bb1369e6493c16e19ec87401658ed29
khochgrafe <k_hochgrafe@hotmail.com>
2026-07-12 21:46:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,89 @@ quoteMessage.textContent = "Great choice! Complete the form below and we will he
     });
 
 });
+
+const galleryImages = document.querySelectorAll(".gallery-image");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImage = document.getElementById("lightbox-image");
+
+const closeLightbox = document.querySelector(".close-lightbox");
+
+const nextButton = document.querySelector(".next");
+
+const prevButton = document.querySelector(".prev");
+
+
+let currentImage = 0;
+
+
+const imageSources = Array.from(galleryImages).map(image => image.src);
+
+
+
+galleryImages.forEach((image, index) => {
+
+    image.addEventListener("click", function(){
+
+        currentImage = index;
+
+        lightboxImage.src = imageSources[currentImage];
+
+        lightbox.style.display = "flex";
+
+    });
+
+});
+
+
+
+nextButton.addEventListener("click", function(){
+
+    currentImage++;
+
+    if(currentImage >= imageSources.length){
+
+        currentImage = 0;
+
+    }
+
+    lightboxImage.src = imageSources[currentImage];
+
+});
+
+
+
+prevButton.addEventListener("click", function(){
+
+    currentImage--;
+
+    if(currentImage < 0){
+
+        currentImage = imageSources.length - 1;
+
+    }
+
+    lightboxImage.src = imageSources[currentImage];
+
+});
+
+
+
+closeLightbox.addEventListener("click", function(){
+
+    lightbox.style.display = "none";
+
+});
+
+
+
+lightbox.addEventListener("click", function(event){
+
+    if(event.target === lightbox){
+
+        lightbox.style.display = "none";
+
+    }
+
+});
